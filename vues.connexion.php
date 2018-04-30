4e488961c5367fdcd20d9179414ec8718e94f9a1
AlbinLH <32388806+AlbinLH@users.noreply.github.com>
2018-04-30 11:38:08 +0200

    Update vues.connexion.php

--- vues.connexion.php
+++ vues.connexion.php
@@ -5,7 +5,7 @@
    </head>
    <body>
       <h2>Connexion au site</h2>
-      <form action="connexion.php" method="post">
+      <form action="modele.connexion.php" method="post">
          <table>
             <tr>
                <td><label for="login"><strong>Nom de compte</strong></label></td>
@@ -18,5 +18,6 @@
          </table>
          <input type="submit" name="connexion" value="Se connecter"/>
       </form>
+      <p><a href="vues.inscription1.php">Inscrivez-vous au service</a></p>
    </body>
 </html>
