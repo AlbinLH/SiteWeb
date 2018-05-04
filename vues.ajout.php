d58361cf42403fd26f30496b40cc3be721129772
AlbinLH <32388806+AlbinLH@users.noreply.github.com>
2018-05-04 11:27:04 +0200

    Update vues.ajout.php

--- vues.ajout.php
+++ vues.ajout.php
@@ -1,19 +1,1 @@
-<?php
 
-?>
-
-<?php echo AfficheAlerte($alerte); ?>
-
-<form method="POST" action="">
-	
-	<label>Nom :</label>
-	<input type="text"  name="name" />
-	
-	<label>Type :</label>
-	<input type="text"  name="type" />
-
-    <button type="submit" name="submit">Ajouter</button>
-
-</form>
-
-<p><a href="index.php?cible=capteurs">Retour</a> | <a href="index.php">Accueil</a></p>
