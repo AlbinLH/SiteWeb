6de012f2ec1cb2a1ca089a14821dfe9fe6778032
kevininore <32388829+kevininore@users.noreply.github.com>
2018-05-04 11:51:03 +0200

    Update vues.ajout.php

--- vues.ajout.php
+++ vues.ajout.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>Lares</title>
-		<link rel="stylesheet" href="vues.ajout.css"/>
+		<link rel="stylesheet" href="capteurs.css"/>
 	</head>
 
 	<body>
 		<div id="conteneur">
-			<div class="element">Définir une nouvelle pièce</div></br>
+			<div class="element"><a href="ajout_de_pieces.php">Définir une nouvelle pièce</a></div></br>
 			<div class="element">Ajouter des capteurs à une pièce existante</div>
 		</div>
 		
