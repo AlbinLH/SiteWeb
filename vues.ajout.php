463ad41fe070d206c7687f9fd4676fbb0d195c34
kevininore <32388829+kevininore@users.noreply.github.com>
2018-05-04 11:31:33 +0200

    Update vues.ajout.php

--- vues.ajout.php
+++ vues.ajout.php
@@ -1,1 +1,17 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Lares</title>
+		<link rel="stylesheet" href="capteurs.css"/>
+	</head>
 
+	<body>
+		<div id="conteneur">
+			<div class="element">Définir une nouvelle pièce</div></br>
+			<div class="element">Ajouter des capteurs à une pièce existante</div>
+		</div>
+		
+	</body>
+
+</html>
