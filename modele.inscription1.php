c2d63dfe5ee3ebea3262fe22f45d18fa60e5a5aa
kevininore <32388829+kevininore@users.noreply.github.com>
2018-05-09 10:30:59 +0200

    Update modele.inscription1.php

--- modele.inscription1.php
+++ modele.inscription1.php
@@ -10,37 +10,34 @@ $gender = $_POST['gender'];
 $numbernameadress = $_POST['numbernameadress'];
 $postalcode = $_POST['postalcode'];
 $city = $_POST['city'];
-$password = $_POST['password']; //a sécuriser
+$password = $_POST['password']; //a sÃ©curiser
 $passwordcheck = $_POST['passwordcheck'];
 $taillelogement = $_POST["taille"];
-
 try {
 $bdd = new PDO('mysql:host=localhost;dbname=users;charset=utf8', 'root', 'root');
 }
 catch(Exception $e) {
-    // En cas d'erreur, on affiche un message et on arrête tout
+    // En cas d'erreur, on affiche un message et on arrÃªte tout
     die('Erreur : '.$e->getMessage());
 }
 if (isset($_POST['mail']) AND isset($_POST['gender']) AND isset($_POST['numbernameadress']) AND isset($_POST['postalcode']) AND isset($_POST['city']) AND isset($_POST['password']) AND isset($_POST['passwordcheck']) AND isset($_POST['taille'])) {
     if ($password==$passwordcheck) {
-        $reponse = $bdd->query(''); //Ajouter les champs à la base de données //Securiser contre les injections sql
-    } 
+        $reponse = $bdd->query(''); //Ajouter les champs Ã  la base de donnÃ©es //Securiser contre les injections sql
+    }
     else {
         echo "Mots de passe non identique"; ?>
     	<a href="vues.inscription.php">Retour</a>
         <?php
     }
-} 
-else {
+}
+else 
     echo "Vous n'avez pas saisi toutes les informations";
     ?>
     <a href="vues.inscription.php">Retour</a>
-}
+
 
 <?php 
-$reponse->closeCursor(); // Termine le traitement de la requête
-
+$reponse->closeCursor(); // Termine le traitement de la requÃªte
 ?>
 	</body>
 </html>
-
