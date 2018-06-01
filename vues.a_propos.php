10f52a824d4da772b20b44fecd63b1d00de2a3e1
comedev <32388830+comedev@users.noreply.github.com>
2018-06-01 10:42:36 +0200

    Add files via upload

---
+++ vues.a_propos.php
@@ -0,0 +1,55 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="vues.a_propos.css">
+</head>
+    
+<body>
+    
+    <div class="main">
+    <h1><strong>À PROPOS</strong></h1><br />
+    <h2><strong>QUI SOMMES-NOUS ?</strong></h2>
+    <p id="qui">blabl abalbalba lbalab labalba lablaba lbalb albalba lbbala bbalbal balb ablab lablabal balba lbalbal alblalbl ablba lbal balbab bbbbb bbbbb bbbbb bbbb bbbb bbbbbb blbal balba lbalbal balbalbalba lbalbalb albalba blbalabla blabalba lbalba</p>
+    
+    <div class="resume">
+        <div id="line_1">
+        <div class="bulle">
+            <div class="icon"><img src="bien.png" alt="simplicite"></div>
+            <p><strong>Simplicité</strong></p>
+            <p>Une interface simple d'utilisation</p>
+        </div>
+        
+        <div class="bulle">
+            <div class="icon"><img src="qualite.png" alt="qualite"></div>
+            <p><strong>Qualité</strong></p>
+            <p>Garantie satisfait ou refait</p>
+        </div>
+
+        <div class="bulle">
+            <div class="icon"><img src="team.png" alt="engagement"></div>
+            <p><strong>Engagement</strong></p>
+            <p>Une équipe impliquée</p>
+        </div>
+        </div>
+
+        <div id="line_2">
+        <div class="bulle">
+            <div class="icon"><img src="tech.png" alt="performance"></div>
+            <p><strong>Performance</strong></p>
+            <p>Une technologie dernier cri</p>
+        </div>
+
+        <div class="bulle">
+            <div class="icon"><img src="securite.png" alt="securite"></div>
+            <p><strong>Sécurité</strong></p>
+            <p>Une sécurité sans failles</p>
+        </div>
+        </div>
+
+    </div>
+    </div>
+    
+</body>
+</html>
---
+++ vues.admin_1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>maPage</title>
+        <link rel="stylesheet" href="vues.admin_1.css" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    </head>
+
+ 	<body>
+        <div id="global">
+     	<h1 class="titre_pers">ESPACE PERSONNEL</h1>
+        <div class="conteneur">
+            <div class="box">
+                <!-- 2x Lien : changer de mot de passe-->
+                <a class="icon" href="#"><img src="key.svg" alt="key"></a>   
+                <a class="txt" href="#">Changer de mot de passe</a>         
+            </div>
+            <div class="box">
+                <!-- 2x Lien : Changer adresse mail-->
+                <a class="icon" href="#"><img src="at.svg" alt="at"></a>
+                <a class="txt" href="#">Changer adresse mail</a>
+            </div>
+            <div class="box">
+                <!-- 2x Lien : Liste utilisateurs bannis-->
+                <a class="icon" href="#"><img src="clipboard-list.svg" alt="clipboard"></a>
+                <a class="txt" href="#">Liste utilisateurs bannis</a>
+            </div>
+            
+        </div>
+        <h1 class="titre_user">ESPACE UTILISATEUR</h1><br /><br />
+        <div class="conteneur_2">
+        <form method="post" action="traitement.php"> <!-- Attention à traitement -->
+            <label for="pays"><strong>Veuillez choisir parmi la liste des utilisateurs gérés suivants : </strong></label>
+                <select name="pays" id="pays">
+                       <option value="france">France</option>
+                       <option value="espagne">Espagne</option>
+                       <option value="italie">Italie</option>
+                       <option value="royaume-uni">Royaume-Uni</option>
+                       <option value="canada">Canada</option>
+                       <option value="etats-unis">États-Unis</option>
+                       <option value="chine">Chine</option>
+                       <option value="japon">Japon</option>
+                </select>
+        </form>
+        
+        <br /><br />
+        <div class="search">
+        <p><strong>Recherche spécifique : </strong></p>
+        <table>
+            <tr>
+                <td><strong>Identifiant</strong></td>
+                <td><strong> : </strong></td>
+                <td> <input type="search" class="recherche"> </td>
+                <td><input type="button" value="Rechercher" class="boutons2"></td>
+            </tr>
+            <tr>
+                <td><strong>Adresse</strong></td>
+                <td><strong> : </strong></td>
+                <td><input type="search" class="recherche"> </td>
+                <td><input type="button" value="Rechercher" class="boutons2"></td>
+            </tr>
+            <tr>
+                <td><strong>Nom</strong></td>
+                <td><strong> : </strong></td>
+                <td><input type="search" class="recherche"></td>
+                <td><input type="button" value="Rechercher" class="boutons2"></td>
+            </tr>
+            <tr>
+                <td><strong>Prénom</strong></td>
+                <td> : </td>
+                <td><input type="search" class="recherche"></td>
+                <td><input type="button" value="Rechercher" class="boutons2"></td>
+            </tr>
+        </table>
+        </div>
+            
+        </div>    
+            
+        </div>
+ 	</body>
+</html>
