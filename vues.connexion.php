bebd78bbf59e1bc0ed7c58b10f423c5e4169afcb
comedev <32388830+comedev@users.noreply.github.com>
2018-06-02 17:57:31 +0200

    Update vues.connexion.php

--- vues.connexion.php
+++ vues.connexion.php
@@ -5,15 +5,17 @@
       <title>Connexion aux services</title>
    </head>
    <body>
-   		<p class="blocktext"><img id="image" src ="LARES.png" alt="logoLARES" /></p>
-      <h2 class="blocktext">Connexion aux services LARES</h2>
-      <form action="modele.connexion.php" method="post">
+   	    <div class="blocktext" id="App-logo"><img id="image" src ="LOGO_1.png" alt="logoLARES" /></div>
+        <h2 class="blocktext">Connexion aux services LARES</h2>
+        <form action="modele.connexion.php" method="post">
          <p class="blocktext"><input type="text" placeholder="E-mail" name="login" id="login"/></p>
          <p class="blocktext"><input type="password" placeholder="Mot de passe" name="pass" id="pass"/></p>
-         <p class="blocktext"><button type="submit" name="connexion" value="Se connecter"/>Valider</button></p>
+         <p class="blocktext"><button type="submit" name="connexion" value="Se connecter" id="val">Valider</button></p>
       </form>
+
       <form action="vues.inscription1.php">
-      	</br><p class="blocktext" id="inscription"><button type="submit" name="submit">Pas encore inscrit ? S'inscrire ici</button></p>
+      	<br /><p class="blocktext" id="inscription"><button type="submit" name="submit">Pas encore inscrit ? S'inscrire ici</button></p>
       </form>
+
    </body>
 </html>
