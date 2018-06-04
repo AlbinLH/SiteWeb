f60d937e93f77646cf410d611bfd7480f0d1ab07
Mathieu Lelièvre <mathieufilou@gmail.com>
2018-06-04 09:57:55 +0200

    Add files via upload

--- vues.a_propos.php
+++ vues.a_propos.php
@@ -11,10 +11,10 @@
     <div class="main">
     <h1><strong>À PROPOS</strong></h1><br />
     <h2><strong>QUI SOMMES-NOUS ?</strong></h2>
-    <p id="qui">Nous sommes Larès, une jeune entreprise composée de 5 étudiants de l'Institut Supérieur d'Electronique de Paris. Nous proposons une expérience domotique couvrant l'ensemble des dimensions d'un tel projet, de l'installation du robot et des capteurs à domicile, à l'application de gestion à distance.</p>
+    <p id="qui">Nous sommes Larès, une jeune entreprise composée de 5 étudiants de l'Institut Supérieur d'Electronique de Paris. Nous proposons une expérience domotique couvrant l'ensemble des dimensions d'un tel projet, de l'installation du robot et des capteurs à domicile, à l'application de gestion à distance. Notre expertise est fondée sur des bases théoriques confirmées, ainsi que des expériences concrètes réussies. Nous mettons un point d'honneur à consulter régulièrement le client, en gérant chaque commande de manière agile, afin d'obtenir un résultat personnalisé qui est sûr de vous plaire.</p>
     <br>
     <h2><strong>POURQUOI NOUS ?</strong></h2>
-    <p id="quo">Notre expertise est fondée sur des bases théoriques confirmées, ainsi que des expériences concrètes réussies. Nous mettons un point d'honneur à consulter régulièrement le client, en gérant chaque commande de manière agile, afin d'obtenir un résultat personnalisé qui est sûr de vous plaire. </p>
+    <p id="quo">Non seulement nous nous engageons à ce notre service soit 100% satisfait ou remboursé, mais nous pouvons affirmer que notre solution est adaptée pour les 7 à 77 ans; pas de failles XSS ou d'injection SQL possible chez nous, la sécurité est primordiale, avec entre autres un chiffrage de bout en bout; des capteurs à la pointe de la technologie, connexion en fibre optique et robot sur-mesure à la clé.</p>
     
     
     <div class="resume">
