d783aa2f1f18981a5c6fae4a340a637d809c47cd
comedev <32388830+comedev@users.noreply.github.com>
2018-06-05 10:05:14 +0200

    Add files via upload

--- vues.profil.php
+++ vues.profil.php
@@ -5,44 +5,44 @@ session_start();
 <!DOCTYPE php>
 <html lang="fr">
 	<head>
-		<link rel="stylesheet" href="vues.connexion.css">
+		<link rel="stylesheet" href="vues.profil.css">
 		<title>Connexion aux services</title>
 	</head>
 	<body>
+        
+    <div class="global">
 	<form method='POST' action="vues.modif.php">
-		
-			<h1 class="text">Votre profil :</h1>
+        <h1 class="text">MON PROFIL</h1><br />
 			
-					<p class="texte">Nom et prénom :</p>
-				
-				<?php 
-			     if ($_SESSION['genre'] == 'Homme') {
-			         echo "Monsieur "; echo $_SESSION['prenom']; echo " ";  echo $_SESSION['nom'];
-			     } else {
-			         echo "Madame "; echo $_SESSION['prenom']; echo " ";  echo $_SESSION['nom']; 
-			     }
-			     ?>
-			   
-					<button type="submit" name="edit" value="changer_nom"/>Editer</button>
-				
-					<p class="texte">Adresse de votre logement :</p>
-						
-					<p><?php echo $_SESSION['adresse']; ?></p>
-				
-					<button type="submit" name="edit" value="changer_adresse"/>Editer</button>
-			
-					<p class="texte">Adresse mail :</p>
-					
-					<p class="texte"><?php echo $_SESSION['adresse_mail']; ?></p>
-				
-					<button type="submit" name="edit" value="changer_adresse_mail"/>Editer</button>
+        <p class="texte">NOM ET PRÉNOM :</p>
+        <p class="info">
+        <?php 
+        if ($_SESSION['genre'] == 'Homme') {
+        echo "Monsieur "; echo $_SESSION['prenom']; echo " ";  echo $_SESSION['nom'];
+        } else {
+        echo "Madame "; echo $_SESSION['prenom']; echo " ";  echo $_SESSION['nom']; 
+        }
+        ?>
+        </p>	     
+        <button class="bouton" type="submit" name="edit" value="changer_nom">Editer</button>
 		
-					<p class="texte">Mot de passe :</p>
-					
-					<p>********</p>
-				
-					<button type="submit" name="edit" value="changer_mot_de_passe"/>Editer</button>
+        <br />
+        <p class="texte">ADRESSE DU LOGEMENT :</p>		
+        <p class="info"><?php echo $_SESSION['adresse']; ?></p>		
+        <button class="bouton" type="submit" name="edit" value="changer_adresse">Editer</button>
+		
+        <br />
+        <p class="texte">ADRESSE MAIL :</p>
+        <p class="info"><?php echo $_SESSION['adresse_mail']; ?></p>
+        <button class="bouton" type="submit" name="edit" value="changer_adresse_mail">Editer</button>
+		
+        <br />
+        <p class="texte">MOT DE PASSE :</p>
+        <p class="info">********</p>
+        <button class="bouton" type="submit" name="edit" value="changer_mot_de_passe">Editer</button>
 		
 	</form>
+    </div>    
+        
 	</body>
 </html>
