e291dd2feaf8d9307e2de48515be0b66abb8dead
comedev <32388830+comedev@users.noreply.github.com>
2018-06-11 10:00:49 +0200

    Add files via upload

--- vues.modif.php
+++ vues.modif.php
@@ -5,38 +5,43 @@
 		<title>Modification de vos données</title>
 	</head>
 <body>
+    
+<div class="global">
+
 <form method="POST" action="modele.modif.php">
 <?php 
 if ($_POST['edit'] == 'changer_nom') {
     ?>
-    	<p class="text">Entrez votre nom et prenom :</p>
-    	<input type='text' name='nom' id='nom'/>
-    	<input type='text' name='prenom' id='prenom'/>
+    	<p class="texte">Entrez votre nom et prenom :</p>
+    	<input type='text' name='nom' id='nom' class="modif"/>
+    	<input type='text' name='prenom' id='prenom' class="modif"/>
     	<?php 
 } else if ($_POST['edit'] == 'changer_adresse') {
-    ?> <p class="text">Entrez votre numéro et nom de rue :</p>
-    	<input type='text' name='rue' id='rue'/>
-    	<p class='text'>Entrez votre code postal</p>
-    	<input type='text' name='code_postal' id='code_postal'/>
-    	<p class="text">Entrez votre ville :</p>
-    	<input type='text' name='ville' id='ville'/>
+    ?> <p class="texte">Entrez votre numéro et nom de rue :</p>
+    	<input type='text' name='rue' id='rue' class="modif"/>
+    	<p class="texte">Entrez votre code postal</p>
+    	<input type='text' name='code_postal' id='code_postal' class="modif"/>
+    	<p class="texte">Entrez votre ville :</p>
+    	<input type='text' name='ville' id='ville' class="modif"/>
     	<?php 
 } else if ($_POST['edit'] == 'changer_adresse_mail') {
-    ?> <p class="text">Entrez votre adresse mail :</p>
-    	<input type='text' name='identifiant' id='identifiant'/>
+    ?> <p class="texte">Entrez votre adresse mail :</p>
+    	<input type='text' name='identifiant' id='identifiant' class="modif"/>
     	<?php 
 } else if ($_POST['edit'] == 'changer_mot_de_passe') {
-    ?> <p class="text">Entrez votre ancien mot de passe :</p>
-    	<input type='password' name='old_mdp' id='old_mdp'/>
-    	<p class="text">Entrez et confirmez votre nouveau mot de passe :</p>
-    	<input type='password' name='mdp1' id='mdp1'/>
-    	<input type='password' name='mdp2' id='mdp2'/>
+    ?> <p class="texte">Entrez votre ancien mot de passe :</p>
+    	<input type='password' name='old_mdp' id='old_mdp' class="modif"/>
+    	<p class="texte">Entrez et confirmez votre nouveau mot de passe :</p>
+    	<input type='password' name='mdp1' id='mdp1' class="modif"/>
+    	<input type='password' name='mdp2' id='mdp2' class="modif"/>
     	<?php 
 }
-//
+
 
 ?>
 <button type="submit" name="submit">Modifier</button>
 </form>
+    
+</div>
 </body>
 </html>
