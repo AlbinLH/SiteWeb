abb43ca863e0324e4bc1bf629f790eb0dc71c808
comedev <32388830+comedev@users.noreply.github.com>
2018-06-13 11:53:53 +0200

    Add files via upload

--- vues.admin2.php
+++ vues.admin2.php
@@ -1,6 +1,3 @@
-<?php 
-session_start();
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,38 +7,54 @@ session_start();
     </head>
 
  	<body>
- 	     <?php 
- 	     $liste=explode(',', $_POST['users']);
- 	     for($i=0; $i<count($liste); $i++){
- 	         echo "<h1> Gestion du compte de  $liste[$i]</h1>";
- 	     }
- 	         ?>          
- 		 <table>
-            <tr>
-                <td>Gestion des capteurs</td>
-                <td><a href="vues.ajoutcapteurs.php"><input type="button" value="Ajouter" class="boutons2"></a></td>
-                <td><a href="vues.ajoutcapteurs.php"><input type="button" value="Supprimer" class="boutons2"></a></td>
-            </tr>
-            <tr>
-                <td>Gestion des actionneurs</td>              
-                <td><input type="button" value="Ajouter" class="boutons2"></td>
-                <td><input type="button" value="Supprimer" class="boutons2"></td>
-            </tr>
-            <tr>
-                <td>Messagerie</td>
-                <td><a href="vues.ajoutcapteurs.php"><input type="button" value="Accès messagerie" class="boutons2"></a></td>
-            </tr>
-            <tr>
-                <td>Action d'urgence</td>
-                <td><a href="vues.donnees_capteurs.php"><input type="button" value="Obtenir donnée capteur" class="boutons2"></a></td>
-                <td><input type="button" value="Déclencher actionneur" class="boutons2"></td>
-            </tr>
-             <tr>
-                <td>Bannir utilisateur</td>
-                <td><input type="button" value="Oui" class="boutons2"></td>
-                <td><input type="button" value="Non" class="boutons2"></td>
-            </tr>
-        </table>
+        <div id="global"> 
+            
+        <h1>GESTION DU COMPTE</h1>
+            
+        <div id="block1">
+            <div class="conteneur1">
+                <p class="text">GESTION DES CAPTEURS</p>
+                <div class="option">
+                    <button class="button1" id="add" title="Add"><img src="plus.svg" alt="plus"></button>
+                    <button class="button1" id="delete" title="Delete"><img src="minus.svg" alt="minus"></button>
+                </div>
+            </div>
+            
+            <div class="conteneur1">
+                <p class="text">GESTION DES ACTIONNEURS</p>
+                <div class="option">
+                    <button class="button1" id="add" title="Add"><img src="plus.svg" alt="plus"></button>
+                    <button class="button1" id="delete" title="Delete"><img src="minus.svg" alt="minus"></button>
+                </div>
+            </div>
+        </div>
+            
+        <div id="block2">
+            <div class="conteneur2">
+                <p class="text">ACTION D'URGENCE</p>
+                <div class="option1">
+                    <button class="button3">Obtenir données capteur</button>
+                    <button class="button3">Déclencher actionneur</button>
+                </div>
+            </div>
+            
+            <div class="conteneur2">
+                <p class="text">BANNIR UTILISATEUR</p>
+                <div class="option1">
+                    <button class="button4" id="yes" title="YES"><img src="check.svg" alt="yes"></button>
+                    <button class="button4" id="no" title="NO"><img src="times.svg" alt="no"></button>
+                </div>
+            </div>
+            
+            <div class="conteneur2">
+                <p class="text">MESSAGERIE</p>
+                <button class="button2">Accès messagerie</button>
+            </div>
+        </div>
+            
+        
+        
+        </div>
 
  	</body>
 </html>
